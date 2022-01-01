f7c3c404843b0fe91d26cca05b07ed2b9123ed5f
Kamen Yotov <kamen@yotov.org>
2022-01-01 15:06:02 -0500

    fix logging

--- main.cc
+++ main.cc
@@ -4,7 +4,10 @@
 DEFINE_string(foo, "a-la-la", "...");
 
 int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  FLAGS_logtostderr = true;
 
   LOG(INFO) << FLAGS_foo;
 
