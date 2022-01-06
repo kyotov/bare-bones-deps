1dc3d1a1a613c017447d7a656de8ec83c6af1333
Kamen Yotov <kamen@yotov.org>
2022-01-06 01:36:51 -0500

    remote cache

---
+++ mode-1b/kydeps/cache.cmake
@@ -0,0 +1,16 @@
+set(KYDEPS_CACHE_BUCKET "https://kydeps.nyc3.digitaloceanspaces.com")
+
+KyDepRegister(
+    d78d5f25460cdd2ca5a159462dfdf0e17f5254e3
+    ${KYDEPS_CACHE_BUCKET}/gflags.d78d5f25460cdd2ca5a159462dfdf0e17f5254e3.zip
+    cbfeca62e761010dd038bbf370f65114146f5611626383093e82c6b69d17b577)
+
+KyDepRegister(
+    a1f98fd2d45bf00587afaefcebc3b2d5c4eacc87
+    ${KYDEPS_CACHE_BUCKET}/glog.a1f98fd2d45bf00587afaefcebc3b2d5c4eacc87.zip
+    129b112eb81970bf1dcf142db2d77c010cba458b1eb12df4b6ef03c3746b3322)
+
+KyDepRegister(
+    c5212d9be90190eaefd79ab9059a05218acfd21d
+    ${KYDEPS_CACHE_BUCKET}/GTest.c5212d9be90190eaefd79ab9059a05218acfd21d.zip
+    9a59237ceba777b30b2d6ed26e84a8cd65fb49500c8eca80e22f72215e5afc36)
