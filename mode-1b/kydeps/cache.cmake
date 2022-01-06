555a4d97749a34d9ef9281bc5fd8673fe54df368
Kamen Yotov <kamen@yotov.org>
2022-01-06 02:08:39 -0500

    cleanup

--- mode-1b/kydeps/cache.cmake
+++ mode-1b/kydeps/cache.cmake
@@ -1,16 +1,68 @@
 set(KYDEPS_CACHE_BUCKET "https://kydeps.nyc3.digitaloceanspaces.com")
 
-KyDepRegister(
-    d78d5f25460cdd2ca5a159462dfdf0e17f5254e3
-    ${KYDEPS_CACHE_BUCKET}/gflags.d78d5f25460cdd2ca5a159462dfdf0e17f5254e3.zip
-    cbfeca62e761010dd038bbf370f65114146f5611626383093e82c6b69d17b577)
 
+#  MANIFEST[gflags]
+#  ----------
+#  CMAKE_BUILD_TYPE=Debug
+#  CMAKE_SYSTEM=Darwin-20.6.0
+#  CMAKE_SYSTEM_PROCESSOR=arm64
+#  ----------
+#  GIT_REPOSITORY
+#  https://github.com/gflags/gflags.git
+#  GIT_TAG
+#  f8a0efe03aa69b3336d8e228b37d4ccb17324b88
+#  CMAKE_ARGS
+#  -DREGISTER_INSTALL_PREFIX=FALSE
+#  -DGFLAGS_BUILD_STATIC_LIBS=TRUE
+#  -DGFLAGS_BUILD_SHARED_LIBS=FALSE
+#  -DGFLAGS_BUILD_TESTING=FALSE
+#  ----------
+#
 KyDepRegister(
-    a1f98fd2d45bf00587afaefcebc3b2d5c4eacc87
-    ${KYDEPS_CACHE_BUCKET}/glog.a1f98fd2d45bf00587afaefcebc3b2d5c4eacc87.zip
-    129b112eb81970bf1dcf142db2d77c010cba458b1eb12df4b6ef03c3746b3322)
-
+    6eaf42029d5dccbe1678f14cd25c610154a01046
+    ${KYDEPS_CACHE_BUCKET}/gflags.6eaf42029d5dccbe1678f14cd25c610154a01046.zip
+    39f47f13f5a691cc7e079ae808c6e775fe82d53b75e9ef6ea06715d14711ee5f
+)
+                
+#  MANIFEST[glog]
+#  ----------
+#  CMAKE_BUILD_TYPE=Debug
+#  CMAKE_SYSTEM=Darwin-20.6.0
+#  CMAKE_SYSTEM_PROCESSOR=arm64
+#  ----------
+#  GIT_REPOSITORY
+#  https://github.com/google/glog.git
+#  GIT_TAG
+#  96a2f23dca4cc7180821ca5f32e526314395d26a
+#  CMAKE_ARGS
+#  -DBUILD_TESTING=FALSE
+#  CMAKE_ARGS
+#  -DCMAKE_PREFIX_PATH:PATH=.../i/gflags.6eaf42029d5dccbe1678f14cd25c610154a01046
+#  DEPENDS
+#  gflags
+#  ----------
+#
 KyDepRegister(
-    c5212d9be90190eaefd79ab9059a05218acfd21d
-    ${KYDEPS_CACHE_BUCKET}/GTest.c5212d9be90190eaefd79ab9059a05218acfd21d.zip
-    9a59237ceba777b30b2d6ed26e84a8cd65fb49500c8eca80e22f72215e5afc36)
+    236475e2004877b4de772255bf8946d01d02f2a7
+    ${KYDEPS_CACHE_BUCKET}/glog.236475e2004877b4de772255bf8946d01d02f2a7.zip
+    97a2aa2012ea47240ec5ca71758932c73fcc9d442f203957541e8f3bc8e964d3
+)
+                
+#  MANIFEST[GTest]
+#  ----------
+#  CMAKE_BUILD_TYPE=Debug
+#  CMAKE_SYSTEM=Darwin-20.6.0
+#  CMAKE_SYSTEM_PROCESSOR=arm64
+#  ----------
+#  GIT_REPOSITORY
+#  https://github.com/google/googletest.git
+#  GIT_TAG
+#  e2239ee6043f73722e7aa812a459f54a28552929
+#  ----------
+#
+KyDepRegister(
+    bf23d61b1cabe072026b445dc322479efdf73b78
+    ${KYDEPS_CACHE_BUCKET}/GTest.bf23d61b1cabe072026b445dc322479efdf73b78.zip
+    597f6d3b4c66803d9b7c6ea33b12c5337f3d5eb46f0fc1d570cac8ebbe2470a4
+)
+                
